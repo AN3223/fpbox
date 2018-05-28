57e0f134dc984af293d87c7f375b6301e5b9812b
AN3223 <35916076+AN3223@users.noreply.github.com>
2018-05-28 00:55:29 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fpbox',
+    version='0.2.0',
+    description='A toolbox for functional programming in Python',
+    url='https://github.com/AN3223/fpbox',
+    author='AN3223',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['build', 'doc', 'dist', 'contrib'])
+)
