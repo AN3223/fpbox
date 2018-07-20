14ac85f65959891e601c51cfbf0e152649f36dac
AN3223 <35916076+AN3223@users.noreply.github.com>
2018-07-20 12:15:13 -0500

    Refactored code

--- fpbox.py
+++ fpbox.py
@@ -104,23 +104,32 @@ class Array(Sequence):
     """
     Immutable homogenous collection. It can be initialized with either a
     single list/tuple/generator (which will return an Array consisting of the
-    contents of said list/tuple/generator) or it can just be given a bunch of
+    contents of said list/tuple/generator) or it can just be given multiple
     arguments to initialize the Array with
     """
 
     def __init__(self, *items):
-        if len(items) == 1:  # Deconstructs single instances of generator and list/tuple
-            if isinstance(head(items), list) or isinstance(head(items), tuple):
+        self.type = type(head(items))
+        items = self._collect(items)
+        self.items = tuple(items)
+        self._check_types()
+
+    def _collect(self, items):
+        if len(items) == 1:
+            if self.type == list or self.type == tuple:
                 items = head(items)
             if isgenerator(head(items)):
-                items = list(head(items))
-        for x in items:  # Checks if all items are the same type
-            if not isinstance(x, type(head(items))):
+                items = tuple(*items)
+            self.type = type(head(items))
+        return items
+
+    def _check_types(self):
+        for x in self.items:
+            if not isinstance(x, self.type):
                 raise FPboxException("You can't mix types in an Array")
-        self.items = tuple(items)
 
     def __repr__(self):
-        if isinstance(head(self.items), Char):  # Creates a representation of [Char]
+        if self.type == Char:  # Creates a representation of [Char]
             unpacked_chars = [x.char for x in self.items]
             return '"{}"'.format(sum(unpacked_chars))
         else:
